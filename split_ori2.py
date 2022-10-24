ce99b03ab4c36660e93c5560cf46d460a6dce65f
Mark Huang <hhzeos@gmail.com>
2022-10-24 22:58:17 +0800

    Update split123

--- split_ori2.py
+++ split_ori2.py
@@ -17,23 +17,6 @@ ROOT = Path(__file__).resolve().parent
 DATA_DIR = ROOT / "data"
 
 
-def revert_uid_map(uids_fp: str):
-    uids_fp = Path(uids_fp)
-    assert uids_fp.exists()
-
-    with open(uids_fp, "r") as f:
-        uids = json.load(f)
-
-    reverse_map = {}
-
-    for key in ["meta_support", "meta_query_ww", "meta_query_wp", "meta_query_pp"]:
-        set_uids = uids[key]
-        for uid in set_uids:
-            reverse_map[uid] = key
-
-    return reverse_map
-
-
 def split_original(
     dataset: str, split_by: str, uids_fp: str, new_split_by: str, overwrite: bool
 ):
@@ -53,16 +36,15 @@ def split_original(
     uids_fp = Path(uids_fp)
     assert uids_fp.exists()
 
-    reverse_map = revert_uid_map(uids_fp)
+    with open(uids_fp, "r") as f:
+        uid_splits = json.load(f)
 
     with open(ref_fp, "rb") as f:
         refs: list = pickle.load(f)
 
     print(f"Loaded {len(refs)} refs from {ref_fp}")
 
-    not_used_sent_counter = 0
-
-    new_refs = []
+    uid_ref_map = {}
     for ref in refs:
         ref_id = ref.get("ref_id")
         num_sent = len(ref.get("sent_ids"))
@@ -72,18 +54,33 @@ def split_original(
 
         for i in range(num_sent):
             uid = f"{ref_id}_{i}"
-            new_split = reverse_map.get(uid)
-            if not new_split:
-                not_used_sent_counter += 1
-                continue
 
             new_ref = copy.deepcopy(ref)
-            new_ref["split"] = new_split
+            # new_ref["split"] = new_split
             new_ref["sent_ids"] = [ref["sent_ids"][i]]
             new_ref["sentences"] = [ref["sentences"][i]]
+            uid_ref_map[uid] = new_ref
+
+    new_refs = []
+    for new_split in [
+        "meta_support",
+        "meta_query_sww",
+        "meta_query_swp",
+        "meta_query_spp",
+        "meta_support_sww",
+        "meta_support_swp",
+        "meta_support_spp",
+    ]:
+        set_uids = uid_splits.get(new_split, [])
+        print(f" >>>>>>>>>>   Split [{new_split}] has {len(set_uids)} uids")
+        for uid in set_uids:
+            new_ref = copy.deepcopy(uid_ref_map.get(uid))
+            if new_ref is None:
+                print_red(f"UID {uid} not found in original refs")
+                continue
+            new_ref["split"] = new_split
             new_refs.append(new_ref)
 
-    print(f"Total {not_used_sent_counter} sentences are not used")
     print(f"Total {len(new_refs)} new refs are generated (with each contains one sent)")
 
     with export_fp.open("wb") as f:
@@ -105,9 +102,12 @@ def check(dataset: str, split_by: str):
     print("Among them:")
     for split_name in [
         "meta_support",
-        "meta_query_ww",
-        "meta_query_wp",
-        "meta_query_pp",
+        "meta_query_sww",
+        "meta_query_swp",
+        "meta_query_spp",
+        "meta_support_sww",
+        "meta_support_swp",
+        "meta_support_spp",
     ]:
         ref_ids = refer.getRefIds(split=split_name)
         print_cyan(f"{len(ref_ids)} refs are in split [{split_name}].")
@@ -131,27 +131,27 @@ def main(split_num: int):
         dataset="refcoco",
         split_by="unc",
         uids_fp=split_dir / "refcoco_unc_train_split_uids.json",
-        new_split_by=f"meta_split{split_num}",
+        new_split_by=f"meta_split{split_num}s",
         overwrite=False,
     )
     split_original(
         dataset="refcoco+",
         split_by="unc",
         uids_fp=split_dir / "refcoco+_unc_train_split_uids.json",
-        new_split_by=f"meta_split{split_num}",
+        new_split_by=f"meta_split{split_num}s",
         overwrite=False,
     )
     split_original(
         dataset="refcocog",
         split_by="umd",
         uids_fp=split_dir / "refcocog_umd_train_split_uids.json",
-        new_split_by=f"meta_split{split_num}",
+        new_split_by=f"meta_split{split_num}s",
         overwrite=False,
     )
 
-    check(dataset="refcoco", split_by=f"meta_split{split_num}")
-    check(dataset="refcoco+", split_by=f"meta_split{split_num}")
-    check(dataset="refcocog", split_by=f"meta_split{split_num}")
+    check(dataset="refcoco", split_by=f"meta_split{split_num}s")
+    check(dataset="refcoco+", split_by=f"meta_split{split_num}s")
+    check(dataset="refcocog", split_by=f"meta_split{split_num}s")
 
 
 if __name__ == "__main__":
