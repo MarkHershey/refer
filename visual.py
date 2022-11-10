6733408fb2686c2c52984bc73bc26a6d0284eb28
Mark Huang <hhzeos@gmail.com>
2022-11-10 15:56:07 +0800

    Update

--- visual.py
+++ visual.py
@@ -25,13 +25,14 @@ def main(dataset: str, splitBy: str, ref_id: int, save_dir: str = "nv-tmp"):
         bbox_inches="tight",
         transparent=True,
     )
+    return str(save_dir / f"{dataset}_{splitBy}_ref{ref_id}.png")
 
 
 def find_ref_id(dataset="refcoco+", splitBy="unc", setName="train", idx: int = None):
     """
     Note that this func assumes that idx supplied is after offset
     """
-    custom_dir = Path("/home/mark/code/novel_composition/data/custom")
+    custom_dir = Path("/home/markhh/CODE/DEEP_LEARNING/novel_composition/data/custom")
     assert custom_dir.exists()
     custom_file = custom_dir / f"{dataset}_{splitBy}_{setName}.csv"
     assert custom_file.exists(), f"{custom_file}"
@@ -75,12 +76,43 @@ def find_orange_juice():
         get("nv-tmp-orange", "refcoco+", "train", i)
 
 
+def find():
+    import json
+
+    fp = "../DEEP_LEARNING/novel_composition/data/c_finetune_refcoco_train.json"
+    with open(fp, "r") as f:
+        data = json.load(f)
+
+    sents = []
+    pics = []
+
+    for idx, sent in data.items():
+        tokens = sent.split()
+        if len(tokens) > 4:
+            continue
+
+        if "coffee" not in tokens:
+            continue
+
+        sents.append(sent)
+        pic = get("p1-3", "refcoco", "train", int(idx))
+        pics.append(pic)
+
+    md_lines = ["| sent | pic |", "| --- | --- |"]
+    for sent, pic in zip(sents, pics):
+        md_lines.append(f"| {sent} | ![]({pic}) |")
+
+    with open("coffee.md", "w") as f:
+        f.write("\n".join(md_lines))
+
+
 if __name__ == "__main__":
     ...
     # main("refcoco+", "unc", ref_id=861)
     # main("refcoco+", "unc", ref_id=19794)
     # main("refcoco+", "unc", ref_id=48870)
-    main("refcoco+", "unc", ref_id=839)
+    # main("refcoco+", "unc", ref_id=839)
+    # main("refcoco", "unc", ref_id=26910)
 
     # get("nv-tmp", "refcoco+", "val", 252014) # 252014 [person] [against wall]
     # get("nv-tmp", "refcoco+", "val", 253890) # 253890 [jumping] [on skate board]
@@ -106,3 +138,20 @@ if __name__ == "__main__":
 
     # get("nv-ww", "refcoco+", "val", 258020) # 258020 [wooden] [seat]
     # get("nv-ww", "refcoco+", "train", 246961) # "246961": "wooden table",
+
+    # get("p1", "refcoco", "train", 74271)
+    # get("p1", "refcoco", "train", 26756)
+    # get("p1", "refcoco", "train", 8134)
+    # get("p1", "refcoco", "train", 60756)
+
+    # get("p1", "refcoco", "train", 90922) # ref 37660
+    # get("p1", "refcoco", "train", 12363) # ref 5080
+    # get("p1", "refcoco", "train", 91197)  # ref 37774
+
+    # get("p1", "refcoco", "train", 33872)  # ref 14015
+    # get("p1", "refcoco", "train", 13826)  # ref 5696
+
+    # get("p1-1", "refcoco", "train", 4111)  # ref
+    # get("p1-1", "refcoco", "train", 7241)  # ref
+    # get("p1-1", "refcoco", "train", 87061)  # ref
+    find()
